069ccc91c41425c2ced586b5b2bf902bfb9ddc01
Maxim <Maxim.like.python@ya.ru>
2019-02-26 02:29:57 +0300

    Time in Logger

--- MyLogger.cpp
+++ MyLogger.cpp
@@ -1,6 +1,8 @@
 ﻿#include "MyLogger.h"
-#include <Windows.h>
 #include <fstream>
+#include <chrono>
+#include <ctime>
+#include <Windows.h>
 #include "python2.7/Python.h"
 
 
@@ -9,12 +11,17 @@ static std::ofstream dbg_log("NY_Event_debug_log.txt", std::ios::app);
 
 void __log_private(char* buf, const char* fmt, ...)
 {
+	using namespace std::chrono;
+	std::time_t t = system_clock::to_time_t(system_clock::now());
+	size_t len = std::strftime(buf, 1024, "%H:%M:%S: ", std::localtime(&t));
+
 	va_list args;
 	va_start(args, fmt);
-	sprintf_s(buf, 1024, fmt, args);
+	sprintf_s(buf + len, 1024 - len, fmt, args);
+	va_end(args);
+
 	OutputDebugStringA(buf);
 	dbg_log << buf << std::flush;
-	va_end(args);
 }
 
 void __log_private_with_pystdout(char* buf, const char* fmt, ...)
